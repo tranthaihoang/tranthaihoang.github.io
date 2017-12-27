827a6e3b51f6a24079a56b99b8f649398e01e9e8
hoangcntt2 <hoangcntt2@gmail.com>
2017-12-27 18:05:20 +0700

    d

---
+++ images/posts/404.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Error 404 | HRMS HOPEE</title>
+	<base href="{{asset('')}}">
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+	<link rel="shortcut icon" type="image/png" href="favicon.png"/>
+	<style>
+            *{
+            transition: all 0.6s;
+				}
+
+				html {
+				    height: 100%;
+				}
+
+				body{
+				    font-family: 'Lato', sans-serif;
+				    color: #888;
+				    margin: 0;
+				}
+
+				#main{
+				    display: table;
+				    width: 100%;
+				    height: 100vh;
+				    text-align: center;
+				}
+
+				.fof{
+					  display: table-cell;
+					  vertical-align: middle;
+				}
+
+				.fof h1{
+					  font-size: 50px;
+					  display: inline-block;
+					  padding-right: 12px;
+					  animation: type .5s alternate infinite;
+				}
+
+				@keyframes type{
+					  from{box-shadow: inset -3px 0px 0px #888;}
+					  to{box-shadow: inset -3px 0px 0px transparent;}
+				}
+
+				a.btn-back-404{
+                     background: #F5F5F5;
+                     padding: 10px 10px;
+                     color: #000;
+                     text-decoration: none;
+                     border-radius: 10px;
+                     box-shadow: 0 4px 6px rgba(50,50,93,.11), 0 1px 3px rgba(0,0,0,.08);
+				}
+	</style> 
+</head>
+<body>
+     <div id="main">
+    	<div class="fof">
+        		<h1>HRMS Error 404</h1>
+        		<div class="clear"></div>
+        		<a class="btn-back-404" href="/">Back Home</a>
+    	</div>
+     </div>
+</body>
+</html>
